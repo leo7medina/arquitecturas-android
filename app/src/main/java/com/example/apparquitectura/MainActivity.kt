8d812910b9d444773f4bc1a56ce85dd719cd6ebe
leo7medina <leo_medina14@hotmail.com>
2022-12-11 15:37:23 -0500

    architecture mvc

--- app/src/main/java/com/example/apparquitectura/MainActivity.kt
+++ app/src/main/java/com/example/apparquitectura/MainActivity.kt
@@ -2,11 +2,50 @@ package com.example.apparquitectura
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
+import com.example.apparquitectura.model.ApiAdapter
+import com.google.gson.JsonElement
+import com.google.gson.JsonObject
+import retrofit2.Call
+import retrofit2.Callback
+import retrofit2.Response
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
         supportActionBar?.hide()
+
+        //VIEW
+        val rvCoupons: RecyclerView = findViewById(R.id.rvCoupons)
+        rvCoupons.layoutManager = LinearLayoutManager(this)
+        val coupons = ArrayList<Coupon>()
+
+        //CONTROLLER
+        val apiAdapter = ApiAdapter();
+        val apiService = apiAdapter.getClientService()
+        val call = apiService.getCoupons()
+
+        call.enqueue(object : Callback<JsonObject> {
+            override fun onFailure(call: Call<JsonObject>, t: Throwable) {
+                t.message?.let { Log.e("ERROR: ", it) }
+                t.stackTrace
+            }
+
+            override fun onResponse(call: Call<JsonObject>, response: Response<JsonObject>) {
+                val offersJsonArray = response.body()?.getAsJsonArray("offers")
+                offersJsonArray?.forEach { jsonElement: JsonElement ->
+                    var jsonObject = jsonElement.asJsonObject
+                    var coupon = Coupon(jsonObject)
+                    coupons.add(coupon)
+                }
+                //VIEW
+                rvCoupons.adapter = RecyclerCouponsAdapter(coupons, R.layout.card_coupon)
+                //VIEW
+            }
+        })
+        //CONTROLLER
     }
 }
